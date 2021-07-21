f0bc16b27b8019ab16a2a639ddc5d9ca397b7f5a
Yeliazar <erik.ruzhakov@mail.ru>
2021-07-22 01:22:18 +0300

    Add toggleMenu in Layout

--- src/HOC/Layout/Layout.js
+++ src/HOC/Layout/Layout.js
@@ -1,10 +1,27 @@
 import React, {Component} from 'react'
 import s from './Layout.module.css'
+import MenuToggle from "../../components/Navigation/MenuToggle";
 
 class Layout extends Component {
+
+    state = {
+        menu: false
+    }
+
+    toggleMenuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+
     render(){
         return (
             <div className={s.Layout}>
+                <MenuToggle
+                    onToggle={this.toggleMenuHandler}
+                    isOpen={this.state.menu}
+                />
+
                 <main>
                     {this.props.children}
                 </main>
