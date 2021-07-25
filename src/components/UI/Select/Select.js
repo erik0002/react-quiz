d266f1639fb986b438c0e5d63b1fc95e365a514f
Yeliazar <erik.ruzhakov@mail.ru>
2021-07-25 17:56:17 +0300

    Customized QuizCreator, create component Select and Auxillary, formFramework

---
+++ src/components/UI/Select/Select.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import s from './Select.module.css'
+
+const Select = props => {
+    const htmlFor = `${props.label} - ${Math.random()}`
+    return (
+        <div className={s.Select}>
+            <label htmlFor={htmlFor}/>
+            <select
+                id={htmlFor}
+                value={props.value}
+                onChange={props.onChange}
+            >
+                { props.options.map((option, index) => {
+                    return (
+                        <option
+                            value={option.value}
+                            key={option.value + index}
+                        >
+                            {option.text}
+                        </option>
+                    )
+                })}
+            </select>
+        </div>
+    )
+}
+
+export default Select
