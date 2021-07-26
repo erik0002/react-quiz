87e531a06b3a92e07b03516c83a8ef02e22847bd
Yeliazar <erik.ruzhakov@mail.ru>
2021-07-26 23:51:44 +0300

    Add Loader Component

---
+++ src/components/UI/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './Loader.module.css';
+
+const Loader = () => (
+    <div className={s.center}>
+        <div className={s.Loader}>
+            <div/><div/>
+        </div>
+    </div>
+)
+
+export default Loader
--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -2,38 +2,60 @@ import React, {Component} from 'react'
 import s from './QuizList.module.css'
 import {NavLink} from "react-router-dom";
 import axios from "axios";
+import Loader from "../../components/UI/Loader/Loader";
 
 export default class QuizList extends Component {
 
+    state = {
+        quizes: [],
+        loading: true
+    }
+
     renderQuizes() {
-        return [1, 2, 3].map((quiz, index) => {
+        return this.state.quizes.map((quiz) => {
               return (
                   <li
-                    key={index}
+                    key={quiz.id}
                   >
-                      <NavLink to={'/quiz/' + quiz}>
-                        Тест {quiz}
+                      <NavLink to={'/quiz/' + quiz.id}>
+                          {quiz.name}
                       </NavLink>
                   </li>
               )
         })
     }
 
-    componentDidMount() {
-        axios.get('https://react-quiz-9452a-default-rtdb.firebaseio.com/quiz.json').then(response => {
-            console.log(response)
-        })
-    }
+    // componentDidMount() {
+    //     axios.get('https://react-quiz-9452a-default-rtdb.firebaseio.com/quiz.json').then(response => {
+    //         console.log(response)
+    //     })
+    // }
+
+       async componentDidMount() {
+            try {
+                const response = await axios.get('https://react-quiz-9452a-default-rtdb.firebaseio.com/quizes.json')
+
+                const quizes = []
+                Object.keys(response.data).forEach((key, index) => {
+                    quizes.push({
+                        id: key,
+                        name: `Тест №${index + 1}`
+                    })
+                })
+                this.setState({
+                    quizes, loading: false
+                })
+            } catch (e) {
+                console.log(e)
+            }
+        }
 
     render (){
         return (
             <div className={s.QuizList}>
                 <div>
                     <h1>Список тестов</h1>
-
-                    <ul>
-                        { this.renderQuizes() }
-                    </ul>
+                    {this.state.loading ? <Loader/> : <ul> {this.renderQuizes()} </ul>}
                 </div>
             </div>
         )
